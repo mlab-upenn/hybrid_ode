2b014c15ad580bec5f2b043bc27d1887dd2afbe4
kabir <kapuri@seas.upenn.edu>
2025-08-29 19:12:29 -0400

    updated the training better results

--- train.py
+++ train.py
@@ -85,9 +85,9 @@ def yaw_error(pred, true):
 
 
 
-def side_slip_error(pred, true):
-    """Shortest-path angular error for side slip angle."""
-    return ((pred - true + jnp.pi/2) % (2 * jnp.pi)) - jnp.pi/2
+# def side_slip_error(pred, true):
+#     """Shortest-path angular error for side slip angle."""
+#     return ((pred - true + jnp.pi/2) % (2 * jnp.pi)) - jnp.pi/2
 
 def loss_function(pred_traj, true_traj):
     """Mean squared error for all states with proper yaw angle handling."""
@@ -97,9 +97,9 @@ def loss_function(pred_traj, true_traj):
     yaw_loss = jnp.mean(yaw_error(yaw_pred, yaw_true) ** 2)
 
 
-    side_slip_pred = pred_traj[..., 5]
-    side_slip_true = true_traj[..., 5]
-    side_slip_loss = jnp.mean(side_slip_error(side_slip_pred, side_slip_true) ** 2)
+    # side_slip_pred = pred_traj[..., 5]
+    # side_slip_true = true_traj[..., 5]
+    # side_slip_loss = jnp.mean(side_slip_error(side_slip_pred, side_slip_true) ** 2)
     
     # Handle all other states with regular MSE
     other_indices = jnp.array([0, 1, 3, 4, 6])  # all except yaw (index 2)
@@ -109,7 +109,7 @@ def loss_function(pred_traj, true_traj):
     
     # Combine losses
     total_loss = other_loss + yaw_loss
-    total_loss += side_slip_loss
+    # total_loss += side_slip_loss
     return total_loss
 
 # def loss_function(pred_traj, true_traj):
